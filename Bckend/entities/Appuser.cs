e96f7a5b58af66bc1161ac2d22ae68cbd5819137
Ahmed Dahir <ahmed_dahir@Ahmeds-MacBook-Pro.local>
2024-08-30 15:45:28 +0100

    commit

--- Bckend/entities/Appuser.cs
+++ Bckend/entities/Appuser.cs
@@ -4,5 +4,7 @@ public class Appuser
 {
     public int Id { get; set; }
     public required string LastName { get; set; }
+    public required byte[] PasswordSalt { get; set; }
+    public required byte[] PasswordHash { get; set; }
     
 }
